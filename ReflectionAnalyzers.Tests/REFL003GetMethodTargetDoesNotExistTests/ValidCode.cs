6719bd99842a70f108ec9a6ae3d76ec4ea600972
Johan Larsson <johan_gavle1@hotmail.com>
2018-09-15 00:07:16 +0200

    More tests.

--- ReflectionAnalyzers.Tests/REFL003GetMethodTargetDoesNotExistTests/ValidCode.cs
+++ ReflectionAnalyzers.Tests/REFL003GetMethodTargetDoesNotExistTests/ValidCode.cs
@@ -26,6 +26,50 @@ namespace RoslynSandbox
         }
 
         [Test]
+        public void GetBar()
+        {
+            var code = @"
+namespace RoslynSandbox
+{
+    class Foo
+    {
+        public Foo()
+        {
+            var methodInfo = typeof(Foo).GetMethod(nameof(this.Bar));
+        }
+
+        public void Bar()
+        {
+        }
+    }
+}";
+            AnalyzerAssert.Valid(Analyzer, code);
+        }
+
+        [Test]
+        public void GetBarOverloaded()
+        {
+            var code = @"
+namespace RoslynSandbox
+{
+    class Foo
+    {
+        public Foo()
+        {
+            var methodInfo = typeof(Foo).GetMethod(nameof(this.Bar));
+        }
+
+        public void Bar()
+        {
+        }
+
+        public int Bar(int i) => i;
+    }
+}";
+            AnalyzerAssert.Valid(Analyzer, code);
+        }
+
+        [Test]
         public void UnknownType()
         {
             var code = @"
