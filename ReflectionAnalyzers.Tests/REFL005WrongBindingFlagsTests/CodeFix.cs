d8bd92b153232529607a30d84096b85272803bd1
JohanLarsson <johan_gavle1@hotmail.com>
2018-09-16 12:53:07 +0200

    Refactor test code.

--- ReflectionAnalyzers.Tests/REFL005WrongBindingFlagsTests/CodeFix.cs
+++ ReflectionAnalyzers.Tests/REFL005WrongBindingFlagsTests/CodeFix.cs
@@ -12,8 +12,19 @@ namespace ReflectionAnalyzers.Tests.REFL005WrongBindingFlagsTests
         private static readonly CodeFixProvider Fix = new ArgumentFix();
         private static readonly ExpectedDiagnostic ExpectedDiagnostic = ExpectedDiagnostic.Create("REFL005");
 
-        [Test]
-        public void GetPrivateBindingFlagsPublic()
+        [TestCase("Static",        "BindingFlags.Public | BindingFlags.Instance",                                "BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly")]
+        [TestCase("Static",        "BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly",    "BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly")]
+        [TestCase("Static",        "BindingFlags.NonPublic | BindingFlags.Static",                               "BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly")]
+        [TestCase("this.Public",   "BindingFlags.Public | BindingFlags.Static",                                  "BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly")]
+        [TestCase("this.Public",   "BindingFlags.NonPublic | BindingFlags.Instance",                             "BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly")]
+        [TestCase("this.Public",   "BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly", "BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly")]
+        [TestCase("this.Public",   "BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly",      "BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly")]
+        [TestCase("this.Private",  "BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly",    "BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly")]
+        [TestCase("this.Private",  "BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.DeclaredOnly",   "BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly")]
+        [TestCase("this.ToString", "BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly",    "BindingFlags.Public | BindingFlags.Instance")]
+        [TestCase("this.ToString", "BindingFlags.NonPublic | BindingFlags.Static",                               "BindingFlags.Public | BindingFlags.Instance")]
+        [TestCase("this.ToString", "BindingFlags.Public | BindingFlags.Static",                                  "BindingFlags.Public | BindingFlags.Instance")]
+        public void GetMethod(string method, string flags, string expected)
         {
             var code = @"
 namespace RoslynSandbox
@@ -24,15 +35,17 @@ namespace RoslynSandbox
     {
         public Foo()
         {
-            var methodInfo = typeof(Foo).GetMethod(nameof(this.Bar), ↓BindingFlags.Instance | BindingFlags.Public);
+            var methodInfo = typeof(Foo).GetMethod(nameof(this.Public), ↓BindingFlags.Public | BindingFlags.Static);
         }
 
-        private void Bar()
-        {
-        }
+        public static int Static() => 0;
+
+        public int Public() => 0;
+
+        private int Private() => 0;
     }
-}";
-
+}".AssertReplace("nameof(this.Public)", $"nameof({method})")
+  .AssertReplace("BindingFlags.Public | BindingFlags.Static", flags);
             var fixedCode = @"
 namespace RoslynSandbox
 {
@@ -42,141 +55,18 @@ namespace RoslynSandbox
     {
         public Foo()
         {
-            var methodInfo = typeof(Foo).GetMethod(nameof(this.Bar), BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+            var methodInfo = typeof(Foo).GetMethod(nameof(this.Public), BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
         }
 
-        private void Bar()
-        {
-        }
+        public static int Static() => 0;
+
+        public int Public() => 0;
+
+        private int Private() => 0;
     }
-}";
-            var message = "There is no member matching the name and binding flags. Expected: BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly.";
-            AnalyzerAssert.CodeFix(Analyzer, Fix, ExpectedDiagnostic.WithMessage(message), code, fixedCode);
-        }
-
-        [Test]
-        public void GetStaticBindingFlagsInstance()
-        {
-            var code = @"
-namespace RoslynSandbox
-{
-    using System.Reflection;
-
-    class Foo
-    {
-        public Foo()
-        {
-            var methodInfo = typeof(Foo).GetMethod(nameof(this.Bar), ↓BindingFlags.Instance | BindingFlags.Public);
-        }
-
-        public static void Bar()
-        {
-        }
-    }
-}";
-            var fixedCode = @"
-namespace RoslynSandbox
-{
-    using System.Reflection;
-
-    class Foo
-    {
-        public Foo()
-        {
-            var methodInfo = typeof(Foo).GetMethod(nameof(this.Bar), BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
-        }
-
-        public static void Bar()
-        {
-        }
-    }
-}";
-            var message = "There is no member matching the name and binding flags. Expected: BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly.";
-            AnalyzerAssert.CodeFix(Analyzer, Fix, ExpectedDiagnostic.WithMessage(message), code, fixedCode);
-        }
-
-        [TestCase("GetMethod(nameof(this.Bar), ↓BindingFlags.Public | BindingFlags.Static)", "BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly")]
-        [TestCase("GetMethod(nameof(this.Bar), ↓BindingFlags.NonPublic | BindingFlags.Instance)", "BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly")]
-        [TestCase("GetMethod(nameof(this.Bar), ↓BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly)", "BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly")]
-        [TestCase("GetMethod(nameof(this.Bar), ↓BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly)", "BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly")]
-        public void GetMethod(string call, string expected)
-        {
-            var code = @"
-namespace RoslynSandbox
-{
-    using System.Reflection;
-
-    class Foo
-    {
-        public Foo()
-        {
-            var methodInfo = typeof(Foo).GetMethod(nameof(this.Bar), ↓BindingFlags.Public | BindingFlags.Static);
-        }
-
-        public void Bar()
-        {
-        }
-    }
-}".AssertReplace("GetMethod(nameof(this.Bar), ↓BindingFlags.Public | BindingFlags.Static)", call);
-            var fixedCode = @"
-namespace RoslynSandbox
-{
-    using System.Reflection;
-
-    class Foo
-    {
-        public Foo()
-        {
-            var methodInfo = typeof(Foo).GetMethod(nameof(this.Bar), BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-        }
-
-        public void Bar()
-        {
-        }
-    }
-}".AssertReplace("BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly", expected);
+}".AssertReplace("nameof(this.Public)", $"nameof({method})")
+  .AssertReplace("BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly", expected);
             var message = $"There is no member matching the name and binding flags. Expected: {expected}.";
-            AnalyzerAssert.CodeFix(Analyzer, Fix, ExpectedDiagnostic.WithMessage(message), code, fixedCode);
-        }
-
-        [Test]
-        public void GetToStringWithDeclaredOnly()
-        {
-            var code = @"
-namespace RoslynSandbox
-{
-    using System.Reflection;
-
-    class Foo
-    {
-        public Foo()
-        {
-            var methodInfo = typeof(Foo).GetMethod(nameof(this.ToString), ↓BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-        }
-
-        public void Bar()
-        {
-        }
-    }
-}";
-            var fixedCode = @"
-namespace RoslynSandbox
-{
-    using System.Reflection;
-
-    class Foo
-    {
-        public Foo()
-        {
-            var methodInfo = typeof(Foo).GetMethod(nameof(this.ToString), BindingFlags.Public | BindingFlags.Instance);
-        }
-
-        public void Bar()
-        {
-        }
-    }
-}";
-            var message = "There is no member matching the name and binding flags. Expected: BindingFlags.Public | BindingFlags.Instance.";
             AnalyzerAssert.CodeFix(Analyzer, Fix, ExpectedDiagnostic.WithMessage(message), code, fixedCode);
         }
     }
