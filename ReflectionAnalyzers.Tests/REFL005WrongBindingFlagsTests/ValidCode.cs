26df5fcfec1bdafa3e52d6df3e4241adb89596cf
JohanLarsson <johan_gavle1@hotmail.com>
2018-09-16 13:16:41 +0200

    More test cases.

--- ReflectionAnalyzers.Tests/REFL005WrongBindingFlagsTests/ValidCode.cs
+++ ReflectionAnalyzers.Tests/REFL005WrongBindingFlagsTests/ValidCode.cs
@@ -9,10 +9,23 @@ namespace ReflectionAnalyzers.Tests.REFL005WrongBindingFlagsTests
         private static readonly DiagnosticAnalyzer Analyzer = new GetMethodAnalyzer();
         private static readonly ExpectedDiagnostic ExpectedDiagnostic = ExpectedDiagnostic.Create("REFL005");
 
+        [TestCase("GetMethod(nameof(this.Public))")]
+        [TestCase("GetMethod(nameof(this.Public, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.FlattenHierarchy | BindingFlags.IgnoreCase))")]
+        [TestCase("GetMethod(nameof(this.Public), BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)")]
+        [TestCase("GetMethod(nameof(this.Public), BindingFlags.Public | BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)")]
         [TestCase("GetMethod(nameof(this.ToString))")]
+        [TestCase("GetMethod(nameof(this.ToString), BindingFlags.Public | BindingFlags.Instance)")]
         [TestCase("GetMethod(nameof(this.ToString), BindingFlags.Instance | BindingFlags.Public)")]
-        [TestCase("GetMethod(nameof(this.ToString), BindingFlags.Instance | BindingFlags.Static |BindingFlags.Public)")]
-        public void GetToString(string call)
+        [TestCase("GetMethod(nameof(this.ToString), BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)")]
+        [TestCase("GetMethod(nameof(this.ToString), BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.FlattenHierarchy | BindingFlags.IgnoreCase)")]
+        [TestCase("GetMethod(nameof(this.GetHashCode))")]
+        [TestCase("GetMethod(nameof(this.GetHashCode), BindingFlags.Public | BindingFlags.Instance)")]
+        [TestCase("GetMethod(nameof(this.Private), BindingFlags.NonPublic | BindingFlags.NonPublic | BindingFlags.Instance)")]
+        [TestCase("GetMethod(nameof(this.Private), BindingFlags.NonPublic | BindingFlags.Instance)")]
+        [TestCase("GetMethod(nameof(this.Private), BindingFlags.Instance | BindingFlags.NonPublic)")]
+        [TestCase("GetMethod(nameof(this.Private), BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly)")]
+        [TestCase("GetMethod(nameof(this.Private), BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.FlattenHierarchy | BindingFlags.IgnoreCase)")]
+        public void GetMethod(string call)
         {
             var code = @"
 namespace RoslynSandbox
@@ -23,38 +36,31 @@ namespace RoslynSandbox
     {
         public Foo()
         {
-            var methodInfo = typeof(Foo).GetMethod(nameof(this.ToString));
+            var methodInfo = typeof(Foo).GetMethod(nameof(this.Public), BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
         }
+
+        public static int Static() => 0;
+
+        public int Public() => 0;
+
+        public override string ToString() => string.Empty;
+
+        private int Private() => 0;
     }
-}".AssertReplace("GetMethod(nameof(this.ToString))", call);
+}".AssertReplace("GetMethod(nameof(this.Public), BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)", call);
             AnalyzerAssert.Valid(Analyzer, ExpectedDiagnostic, code);
         }
 
-        [Test]
-        public void GetPrivate()
-        {
-            var code = @"
-namespace RoslynSandbox
-{
-    using System.Reflection;
-
-    class Foo
-    {
-        public Foo()
-        {
-            var methodInfo = typeof(Foo).GetMethod(nameof(this.Bar), BindingFlags.Instance | BindingFlags.NonPublic);
-        }
-
-        private void Bar()
-        {
-        }
-    }
-}";
-            AnalyzerAssert.Valid(Analyzer, ExpectedDiagnostic, code);
-        }
-
-        [Test]
-        public void UnknownType()
+        [TestCase("GetMethod(\"Bar\")")]
+        [TestCase("GetMethod(\"Bar\", BindingFlags.Public | BindingFlags.Instance)")]
+        [TestCase("GetMethod(\"Bar\", BindingFlags.NonPublic | BindingFlags.Instance)")]
+        [TestCase("GetMethod(\"Bar\", BindingFlags.Public | BindingFlags.Static)")]
+        [TestCase("GetMethod(\"Bar\", BindingFlags.NonPublic | BindingFlags.Static)")]
+        [TestCase("GetMethod(\"Bar\", BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance)")]
+        [TestCase("GetMethod(\"Bar\", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance)")]
+        [TestCase("GetMethod(\"Bar\", BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance | BindingFlags.DeclaredOnly)")]
+        [TestCase("GetMethod(\"Bar\", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance | BindingFlags.DeclaredOnly)")]
+        public void GetMethodUnknownType(string call)
         {
             var code = @"
 namespace RoslynSandbox
@@ -69,7 +75,7 @@ namespace RoslynSandbox
             var methodInfo = type.GetMethod(""Bar"");
         }
     }
-}";
+}".AssertReplace("GetMethod(\"Bar\")", call);
             AnalyzerAssert.Valid(Analyzer, code);
         }
     }
