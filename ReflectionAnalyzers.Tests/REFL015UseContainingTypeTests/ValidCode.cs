1d501c3689dd0725549089ca330e710edc69371a
JohanLarsson <johan_gavle1@hotmail.com>
2018-09-21 12:35:38 +0200

    More test cases.

--- ReflectionAnalyzers.Tests/REFL015UseContainingTypeTests/ValidCode.cs
+++ ReflectionAnalyzers.Tests/REFL015UseContainingTypeTests/ValidCode.cs
@@ -17,6 +17,10 @@ namespace ReflectionAnalyzers.Tests.REFL015UseContainingTypeTests
         [TestCase("typeof(Foo).GetEvent(nameof(FooBase.InternalStaticEvent), BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.FlattenHierarchy)")]
         [TestCase("typeof(Foo).GetProperty(nameof(FooBase.InternalStaticProperty), BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.FlattenHierarchy)")]
         [TestCase("typeof(Foo).GetMethod(nameof(FooBase.InternalStaticMethod), BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.FlattenHierarchy)")]
+        [TestCase("typeof(FooBase).GetField(\"PrivateStaticField\", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.FlattenHierarchy)")]
+        [TestCase("typeof(FooBase).GetEvent(\"PrivateStaticEvent\", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.FlattenHierarchy)")]
+        [TestCase("typeof(FooBase).GetProperty(\"PrivateStaticProperty\", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.FlattenHierarchy)")]
+        [TestCase("typeof(FooBase).GetMethod(\"PrivateStaticMethod\", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.FlattenHierarchy)")]
         [TestCase("typeof(FooBase).GetField(nameof(FooBase.PublicStaticField), BindingFlags.Public | BindingFlags.Static)")]
         [TestCase("typeof(FooBase).GetEvent(nameof(FooBase.PublicStaticEvent), BindingFlags.Public | BindingFlags.Static)")]
         [TestCase("typeof(FooBase).GetProperty(nameof(FooBase.PublicStaticProperty), BindingFlags.Public | BindingFlags.Static)")]
@@ -43,17 +47,25 @@ namespace RoslynSandbox
 
         internal static int InternalStaticField;
 
+        private static int PrivateStaticField;
+
         public static event EventHandler PublicStaticEvent;
 
         internal static event EventHandler InternalStaticEvent;
+
+        private static event EventHandler PrivateStaticEvent;
 
         public static int PublicStaticProperty { get; set; }
 
         internal static int InternalStaticProperty { get; set; }
 
+        private static int PrivateStaticProperty { get; set; }
+
         public static int PublicStaticMethod() => 0;
 
         internal static int InternalStaticMethod() => 0;
+
+        private static int PrivateStaticMethod() => 0;
     }
 
     public class Foo : FooBase
