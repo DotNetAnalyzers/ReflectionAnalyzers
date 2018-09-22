265251227078683563a1c40c4bd6d80a75ad67a1
Johan Larsson <johan_gavle1@hotmail.com>
2018-09-22 21:20:45 +0200

    More valid code.

---
+++ ValidCode/NotInSource.cs
@@ -0,0 +1,20 @@
+namespace ValidCode
+{
+    using System;
+    using System.Reflection;
+
+    public class CustomAggregateException : AggregateException
+    {
+        private readonly int value;
+    }
+
+    class NotInSource
+    {
+        public NotInSource()
+        {
+            _ = typeof(AggregateException).GetProperty("InnerExceptionCount", BindingFlags.NonPublic | BindingFlags.Instance);
+            _ = typeof(AggregateException).GetProperty("InnerExceptionCount", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+            _ = typeof(CustomAggregateException).GetProperty("InnerExceptionCount", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+    }
+}
