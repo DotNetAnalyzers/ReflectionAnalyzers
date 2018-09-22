5d6be82954f0a7fd478060542ddb68e85138afc5
JohanLarsson <johan_gavle1@hotmail.com>
2018-09-22 14:24:42 +0200

    Handle local target type. #69.

--- ReflectionAnalyzers.Tests/Helpers/GetXTests.cs
+++ ReflectionAnalyzers.Tests/Helpers/GetXTests.cs
@@ -8,13 +8,13 @@ namespace ReflectionAnalyzers.Tests.Helpers
 
     public class GetXTests
     {
-        [TestCase("typeof(Foo).GetMethod(nameof(this.ToString))",                                       "Foo")]
-        [TestCase("new Foo().GetType().GetMethod(nameof(this.ToString))",                               "Foo")]
-        [TestCase("this.GetType().GetMethod(nameof(this.ToString))",                                    "Foo")]
-        [TestCase("GetType().GetMethod(nameof(this.ToString))",                                         "Foo")]
-        [TestCase("typeof(string).Assembly.GetType(\"System.Int32\").GetMethod(nameof(this.ToString))", "Int32")]
+        [TestCase("typeof(Foo).GetMethod(nameof(this.ToString))",                                                                             "Foo")]
+        [TestCase("new Foo().GetType().GetMethod(nameof(this.ToString))",                                                                     "Foo")]
+        [TestCase("this.GetType().GetMethod(nameof(this.ToString))",                                                                          "Foo")]
+        [TestCase("GetType().GetMethod(nameof(this.ToString))",                                                                               "Foo")]
+        [TestCase("typeof(string).Assembly.GetType(\"System.Int32\").GetMethod(nameof(this.ToString))",                                       "Int32")]
         [TestCase("typeof(IEnumerable<int>).Assembly.GetType(\"System.Collections.Generic.IEnumerable`1\").GetMethod(nameof(this.ToString))", "IEnumerable`1")]
-        public void TryGetTargetType(string call, string expected)
+        public void TryGetTargetTypeExpression(string call, string expected)
         {
             var code = @"
 namespace RoslynSandbox
@@ -36,6 +36,62 @@ namespace RoslynSandbox
             var node = syntaxTree.FindInvocation(call);
             Assert.AreEqual(true, GetX.TryGetTargetType(node, semanticModel, CancellationToken.None, out var type));
             Assert.AreEqual(expected, type.MetadataName);
+        }
+
+        [TestCase("typeof(Foo)",                                                                             "Foo")]
+        [TestCase("new Foo().GetType()",                                                                     "Foo")]
+        [TestCase("this.GetType()",                                                                          "Foo")]
+        [TestCase("GetType()",                                                                               "Foo")]
+        [TestCase("typeof(string).Assembly.GetType(\"System.Int32\")",                                       "Int32")]
+        [TestCase("typeof(IEnumerable<int>).Assembly.GetType(\"System.Collections.Generic.IEnumerable`1\")", "IEnumerable`1")]
+        public void TryGetTargetTypeLocal(string typeExpression, string expected)
+        {
+            var code = @"
+namespace RoslynSandbox
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    class Foo
+    {
+        public Foo()
+        {
+            var type = typeof(Foo);
+            var methodInfo = type.GetMethod(nameof(this.ToString));
+        }
+    }
+}".AssertReplace("typeof(Foo)", typeExpression);
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+            var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+            var node = syntaxTree.FindInvocation("GetMethod");
+            Assert.AreEqual(true, GetX.TryGetTargetType(node, semanticModel, CancellationToken.None, out var type));
+            Assert.AreEqual(expected, type.MetadataName);
+        }
+
+        [Test]
+        public void Recursion()
+        {
+            var code = @"
+namespace RoslynSandbox
+{
+    using System;
+
+    class Foo
+    {
+        public Foo()
+        {
+            Type type;
+            type = type;
+            var methodInfo = type.GetMethod(nameof(this.ToString));
+        }
+    }
+}";
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+            var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+            var node = syntaxTree.FindInvocation("GetMethod");
+            Assert.AreEqual(false,     GetX.TryGetTargetType(node, semanticModel, CancellationToken.None, out _));
         }
 
         [Test]
--- ReflectionAnalyzers/Helpers/GetX.cs
+++ ReflectionAnalyzers/Helpers/GetX.cs
@@ -20,46 +20,58 @@ namespace ReflectionAnalyzers
         /// <returns>True if the type could be determined.</returns>
         internal static bool TryGetTargetType(InvocationExpressionSyntax invocation, SemanticModel semanticModel, CancellationToken cancellationToken, out ITypeSymbol result)
         {
-            if (invocation.Expression is MemberAccessExpressionSyntax memberAccess)
-            {
-                switch (memberAccess.Expression)
-                {
-                    case TypeOfExpressionSyntax typeOf:
-                        return semanticModel.TryGetType(typeOf.Type, cancellationToken, out result);
-                    case InvocationExpressionSyntax getType when getType.TryGetMethodName(out var name) &&
-                                                                 name == "GetType" &&
-                                                                 getType.ArgumentList is ArgumentListSyntax args:
-                        if (args.Arguments.Count == 0)
-                        {
-                            switch (getType.Expression)
-                            {
-                                case MemberAccessExpressionSyntax typeAccess:
-                                    return semanticModel.TryGetType(typeAccess.Expression, cancellationToken, out result);
-                                case IdentifierNameSyntax _ when invocation.TryFirstAncestor(out TypeDeclarationSyntax containingType):
-                                    return semanticModel.TryGetSymbol(containingType, cancellationToken, out result);
-                            }
-                        }
-                        else if (args.Arguments.TrySingle(out var arg) &&
-                                 arg.TryGetStringValue(semanticModel, cancellationToken, out var typeName) &&
-                                 getType.TryGetTarget(KnownSymbol.Assembly.GetType, semanticModel, cancellationToken, out _))
-                        {
-                            switch (getType.Expression)
-                            {
-                                case MemberAccessExpressionSyntax typeAccess when semanticModel.TryGetType(typeAccess.Expression, cancellationToken, out var typeInAssembly):
-                                    result = typeInAssembly.ContainingAssembly.GetTypeByMetadataName(typeName);
-                                    return result != null;
-                                case IdentifierNameSyntax _ when invocation.TryFirstAncestor(out TypeDeclarationSyntax containingType) &&
-                                                                semanticModel.TryGetSymbol(containingType, cancellationToken, out var typeInAssembly):
-                                    result = typeInAssembly.ContainingAssembly.GetTypeByMetadataName(typeName);
-                                    return result != null;
-                            }
-                        }
+            result = null;
+            return invocation.Expression is MemberAccessExpressionSyntax memberAccess &&
+                   TryGetTargetType(memberAccess.Expression, semanticModel, null, cancellationToken, out result);
+        }
 
-                        break;
-                }
+        private static bool TryGetTargetType(ExpressionSyntax expression, SemanticModel semanticModel, PooledSet<ExpressionSyntax> visited, CancellationToken cancellationToken, out ITypeSymbol result)
+        {
+            result = null;
+            switch (expression)
+            {
+                case IdentifierNameSyntax identifierName when semanticModel.TryGetSymbol(identifierName, cancellationToken, out ILocalSymbol local):
+                    using (visited = visited.IncrementUsage())
+                    {
+                        return AssignedValueWalker.TryGetSingle(local, semanticModel, cancellationToken, out var assignedValue) &&
+                               visited.Add(assignedValue) &&
+                               TryGetTargetType(assignedValue, semanticModel, visited, cancellationToken, out result);
+                    }
+
+                case TypeOfExpressionSyntax typeOf:
+                    return semanticModel.TryGetType(typeOf.Type, cancellationToken, out result);
+                case InvocationExpressionSyntax getType when getType.TryGetMethodName(out var name) &&
+                                                 name == "GetType" &&
+                                                 getType.ArgumentList is ArgumentListSyntax args:
+                    if (args.Arguments.Count == 0)
+                    {
+                        switch (getType.Expression)
+                        {
+                            case MemberAccessExpressionSyntax typeAccess:
+                                return semanticModel.TryGetType(typeAccess.Expression, cancellationToken, out result);
+                            case IdentifierNameSyntax _ when expression.TryFirstAncestor(out TypeDeclarationSyntax containingType):
+                                return semanticModel.TryGetSymbol(containingType, cancellationToken, out result);
+                        }
+                    }
+                    else if (args.Arguments.TrySingle(out var arg) &&
+                             arg.TryGetStringValue(semanticModel, cancellationToken, out var typeName) &&
+                             getType.TryGetTarget(KnownSymbol.Assembly.GetType, semanticModel, cancellationToken, out _))
+                    {
+                        switch (getType.Expression)
+                        {
+                            case MemberAccessExpressionSyntax typeAccess when semanticModel.TryGetType(typeAccess.Expression, cancellationToken, out var typeInAssembly):
+                                result = typeInAssembly.ContainingAssembly.GetTypeByMetadataName(typeName);
+                                return result != null;
+                            case IdentifierNameSyntax _ when expression.TryFirstAncestor(out TypeDeclarationSyntax containingType) &&
+                                                            semanticModel.TryGetSymbol(containingType, cancellationToken, out var typeInAssembly):
+                                result = typeInAssembly.ContainingAssembly.GetTypeByMetadataName(typeName);
+                                return result != null;
+                        }
+                    }
+
+                    break;
             }
 
-            result = null;
             return false;
         }
     }
