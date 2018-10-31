6f3345f24b28ac339931d0866d450d1178652372
JohanLarsson <johan_gavle1@hotmail.com>
2018-10-31 11:23:36 +0100

    Delegate.CreateDelegate understand GetMethod & SetMethod. Fix #178

--- ReflectionAnalyzers/Helpers/Reflection/MethodInfo.cs
+++ ReflectionAnalyzers/Helpers/Reflection/MethodInfo.cs
@@ -1,5 +1,6 @@
 namespace ReflectionAnalyzers
 {
+    using Gu.Roslyn.AnalyzerExtensions;
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Diagnostics;
@@ -17,12 +18,39 @@ namespace ReflectionAnalyzers
 
         internal static bool TryGet(ExpressionSyntax expression, SyntaxNodeAnalysisContext context, out MethodInfo methodInfo)
         {
-            if (expression is InvocationExpressionSyntax invocation &&
-                GetX.TryMatchGetMethod(invocation, context, out var member, out _, out _, out _) &&
-                member.Symbol is IMethodSymbol method)
+            switch (expression)
             {
-                methodInfo = new MethodInfo(member.ReflectedType, method);
-                return true;
+                case InvocationExpressionSyntax invocation when GetX.TryMatchGetMethod(invocation, context, out var member, out _, out _, out _) && member.Symbol is IMethodSymbol method:
+                    methodInfo = new MethodInfo(member.ReflectedType, method);
+                    return true;
+                case InvocationExpressionSyntax invocation when invocation.Expression is MemberAccessExpressionSyntax memberAccess &&
+                                                                context.SemanticModel.TryGetSymbol(invocation, KnownSymbol.PropertyInfo.GetGetMethod, context.CancellationToken, out _) &&
+                                                                PropertyInfo.TryGet(memberAccess.Expression, context, out var propertyInfo) &&
+                                                                propertyInfo.Property.GetMethod is IMethodSymbol getMethod:
+                    methodInfo = new MethodInfo(propertyInfo.ReflectedType, getMethod);
+                    return true;
+                case InvocationExpressionSyntax invocation when invocation.Expression is MemberAccessExpressionSyntax memberAccess &&
+                                                                context.SemanticModel.TryGetSymbol(invocation, KnownSymbol.PropertyInfo.GetSetMethod, context.CancellationToken, out _) &&
+                                                                PropertyInfo.TryGet(memberAccess.Expression, context, out var propertyInfo) &&
+                                                                propertyInfo.Property.SetMethod is IMethodSymbol setMethod:
+                    methodInfo = new MethodInfo(propertyInfo.ReflectedType, setMethod);
+                    return true;
+                case MemberAccessExpressionSyntax memberAccess when context.SemanticModel.TryGetSymbol(memberAccess, context.CancellationToken, out ISymbol symbol):
+                    if (symbol == KnownSymbol.PropertyInfo.GetMethod &&
+                        PropertyInfo.TryGet(memberAccess.Expression, context, out var property))
+                    {
+                            methodInfo = new MethodInfo(property.ReflectedType, property.Property.GetMethod);
+                            return true;
+                    }
+
+                    if (symbol == KnownSymbol.PropertyInfo.SetMethod &&
+                        PropertyInfo.TryGet(memberAccess.Expression, context, out property))
+                    {
+                        methodInfo = new MethodInfo(property.ReflectedType, property.Property.SetMethod);
+                        return true;
+                    }
+
+                    break;
             }
 
             methodInfo = default(MethodInfo);
