4867cdf4656e4156c7845bf85d55b4aaed28a274
jaggedSpire <13804530+jaggedSpire@users.noreply.github.com>
2018-11-18 02:58:34 -0600

    check base class members for invokation target, neaten, add doc comments to helper methods

--- ReflectionAnalyzers/NodeAnalzers/DefaultMemberAttributeAnalyzer.cs
+++ ReflectionAnalyzers/NodeAnalzers/DefaultMemberAttributeAnalyzer.cs
@@ -27,35 +27,129 @@ namespace ReflectionAnalyzers
                 return;
             }
 
-            if (!(context.Node is AttributeSyntax attribute))
+            if (!TryGetAttributeAndTypeInfo(context, out string memberName, out Location location, out ITypeSymbol symbol))
             {
                 return;
             }
 
-            if (!Attribute.IsType(attribute, KnownSymbol.DefaultMemberAttribute, context.SemanticModel, context.CancellationToken))
+            if (!IsInvokableMember(symbol, memberName))
             {
-                return;
+                context.ReportDiagnostic(Diagnostic.Create(REFL046DefaultMemberMustExist.Descriptor, location));
+            }
+        }
+
+        /// <summary>
+        /// Attempt to retrieve information about the application of the
+        /// DefaultMemberAttribute:
+        ///
+        ///  - Specified member name
+        ///  - Location of attribute
+        ///  - Type symbol of type with attribute applied
+        ///
+        /// Does not throw on failure.
+        /// </summary>
+        /// <param name="context">Context of attribute application.</param>
+        /// <param name="memberName">Member name specified by attribute.</param>
+        /// <param name="location">Location of attribute application.</param>
+        /// <param name="typeSymbol">Attribute target.</param>
+        /// <returns>Success</returns>
+        private static bool TryGetAttributeAndTypeInfo(
+            SyntaxNodeAnalysisContext context,
+            out string memberName,
+            out Location location,
+            out ITypeSymbol typeSymbol)
+        {
+            memberName = default(string);
+            location = default(Location);
+            typeSymbol = default(ITypeSymbol);
+
+            if (!(context.Node is AttributeSyntax attribute) ||
+                !Attribute.IsType(attribute, KnownSymbol.DefaultMemberAttribute, context.SemanticModel, context.CancellationToken))
+            {
+                return false;
             }
 
-            if (!Attribute.TryFindArgument(attribute, 0, "memberName", out var argument))
+            if (!Attribute.TryFindArgument(attribute, 0, "memberName", out var argument) ||
+                !context.SemanticModel.TryGetConstantValue(argument.Expression, context.CancellationToken, out string workingMemberName))
             {
-                return;
+                return false;
             }
 
-            if (!context.SemanticModel.TryGetConstantValue(argument.Expression, context.CancellationToken, out string memberName))
+            if (!attribute.TryFirstAncestor(out ClassDeclarationSyntax classDeclaration))
             {
-                return;
+                return false;
             }
 
-            if (!attribute.TryFirstAncestor(out ClassDeclarationSyntax classDeclaration)) { return; }
+            if (!context.SemanticModel.TryGetSymbol(classDeclaration, context.CancellationToken, out ITypeSymbol workingTypeSymbol))
+            {
+                return false;
+            }
 
-            // try to find the member
-            if (classDeclaration.TryFindMethod(memberName, out var memberNode)) { return; }
-            if (classDeclaration.TryFindProperty(memberName, out var propertyNode)) { return; }
-            if (classDeclaration.TryFindField(memberName, out var fieldNode)) { return; }
-            if (classDeclaration.TryFindEvent(memberName, out var eventNode)) { return; }
+            memberName = workingMemberName;
+            location = argument.GetLocation();
+            typeSymbol = workingTypeSymbol;
+            return true;
+        }
 
-            context.ReportDiagnostic(Diagnostic.Create(REFL046DefaultMemberMustExist.Descriptor, argument.GetLocation()));
+        /// <summary>
+        /// Check if Type.InvokeMember(memberName, ...) will work with the type
+        /// defined by `symbol` and the supplied member name, assuming the
+        /// other arguments are correct.
+        /// </summary>
+        /// <param name="symbol">symbol of type to call InvokeMember on.</param>
+        /// <param name="memberName">
+        /// Name to be implicitly passed in to InvokeMember when `name`
+        /// argument is an empty string.
+        /// </param>
+        /// <returns>whether the supplied member name is valid for the type.</returns>
+        private static bool IsInvokableMember(ITypeSymbol symbol, string memberName)
+        {
+            // invoke is not supported with base class constructors on derived
+            // types, so we should only check for a constructor match on the
+            // type which actually has the DefaultMember attribute.
+            if (symbol.Name == memberName)
+            {
+                return true;
+            }
+
+            return IsInvokableNonConstructor(symbol, memberName);
+        }
+
+        /// <summary>
+        /// Check if Type.InvokeMember(memberName, ...) will work with the type
+        /// defined by `symbol` and the supplied member name, assuming the
+        /// other arguments are correct, and the invokeAttr does not contain
+        /// BindingFlags.CreateInstace.
+        /// </summary>
+        /// <param name="symbol">symbol of type to call InvokeMember on.</param>
+        /// <param name="memberName">
+        /// Name to be implicitly passed in to InvokeMember when `name`
+        /// argument is an empty string.
+        /// </param>
+        /// <returns>whether the supplied member name is valid for the type.</returns>
+        private static bool IsInvokableNonConstructor(ITypeSymbol symbol, string memberName)
+        {
+            var matches = symbol.GetMembers(memberName);
+
+            if (matches.Any(IsInvokableMember))
+            {
+                return true;
+            }
+
+            if (symbol.BaseType == null)
+            {
+                return false;
+            }
+
+            return IsInvokableNonConstructor(symbol.BaseType, memberName);
+
+            bool IsInvokableMember(ISymbol member)
+            {
+                return
+                    member is IFieldSymbol ||
+                    member is IPropertySymbol ||
+                    member is IMethodSymbol;
+            }
         }
     }
 }
