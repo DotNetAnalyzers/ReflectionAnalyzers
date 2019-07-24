a9c42d6fe9a7ef63e6873ba26794917df68bef50
Johan Larsson <johan_gavle1@hotmail.com>
2019-07-24 02:57:30 +0200

    Fix warnings.

--- ValidCode/UsingStatic.cs
+++ ValidCode/UsingStatic.cs
@@ -1,19 +1,19 @@
 // ReSharper disable All
 namespace ValidCode
 {
-    using NUnit.Framework;
     using System;
     using System.Reflection;
+    using NUnit.Framework;
     using static System.Reflection.BindingFlags;
 
-    class Foo
+    class UsingStatic
     {
         [Test]
         public void Valid()
         {
-            Assert.NotNull(typeof(Foo).GetMethod(nameof(this.Bar), Public | Instance | DeclaredOnly, null, Type.EmptyTypes, null));
-            Assert.NotNull(typeof(Foo).GetMethod(nameof(this.Bar), Public | BindingFlags.Instance | DeclaredOnly, null, Type.EmptyTypes, null));
-            Assert.NotNull(typeof(Foo).GetMethod(nameof(this.Bar), Public | System.Reflection.BindingFlags.Instance | DeclaredOnly, null, Type.EmptyTypes, null));
+            Assert.NotNull(typeof(UsingStatic).GetMethod(nameof(this.Bar), Public | Instance | DeclaredOnly, null, Type.EmptyTypes, null));
+            Assert.NotNull(typeof(UsingStatic).GetMethod(nameof(this.Bar), Public | BindingFlags.Instance | DeclaredOnly, null, Type.EmptyTypes, null));
+            Assert.NotNull(typeof(UsingStatic).GetMethod(nameof(this.Bar), Public | System.Reflection.BindingFlags.Instance | DeclaredOnly, null, Type.EmptyTypes, null));
         }
 
         public int Bar() => 0;
