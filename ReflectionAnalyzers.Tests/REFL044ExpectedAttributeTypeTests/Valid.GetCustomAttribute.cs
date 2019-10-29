b85c3df7eec8c7bbdcbf0363f69ace929e2e3d62
JohanLarsson <johan_gavle1@hotmail.com>
2019-10-29 09:37:55 +0100

    Rename files.

--- ReflectionAnalyzers.Tests/REFL044ExpectedAttributeTypeTests/Valid.GetCustomAttribute.cs
+++ ReflectionAnalyzers.Tests/REFL044ExpectedAttributeTypeTests/Valid.GetCustomAttribute.cs
@@ -5,7 +5,7 @@ namespace ReflectionAnalyzers.Tests.REFL044ExpectedAttributeTypeTests
     using Microsoft.CodeAnalysis.Diagnostics;
     using NUnit.Framework;
 
-    public static partial class ValidCode
+    public static partial class Valid
     {
         public static class GetCustomAttribute
         {
