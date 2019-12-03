49502b96bdc5e3b1e45105f9c5d37eaedfc988c6
JohanLarsson <johan_gavle1@hotmail.com>
2019-12-03 13:44:42 +0100

    Dynamic valid code.

---
+++ ValidCode/Dynamic.cs
@@ -0,0 +1,17 @@
+﻿namespace ValidCode
+{
+    using System.Collections.Generic;
+    using System.Dynamic;
+    using NUnit.Framework;
+
+    public class Dynamic
+    {
+        [Test]
+        public void Index()
+        {
+            dynamic expando = new ExpandoObject();
+            expando.name = "John";
+            Assert.AreEqual("John", ((IDictionary<string, object>)expando)["name"]);
+        }
+    }
+}
