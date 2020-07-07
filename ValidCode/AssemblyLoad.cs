45d303f8ecba809cdcd7bbe870cc2a87a87ffe2d
JohanLarsson <johan_gavle1@hotmail.com>
2020-07-07 09:37:33 +0200

    More valid code.

--- ValidCode/AssemblyLoad.cs
+++ ValidCode/AssemblyLoad.cs
@@ -7,6 +7,7 @@ namespace ValidCode
     {
         public AssemblyLoad()
         {
+            _ = (string)Assembly.Load("SomeAssembly").GetType("SomeType")?.GetMethod("SomeMethod").Invoke(null, new object[] { 1 })!;
             _ = (string?)Assembly.Load("SomeAssembly").GetType("SomeType")?.GetMethod("SomeMethod").Invoke(null, new object[] { 1 });
             _ = (string)Assembly.Load("SomeAssembly").GetType("SomeType", throwOnError: true).GetMethod("SomeMethod").Invoke(null, new object[] { 1 });
             _ = (string)Assembly.Load("SomeAssembly, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e000\"").GetType("SomeType", throwOnError: true).GetMethod("SomeMethod").Invoke(null, new object[] { 1 });
