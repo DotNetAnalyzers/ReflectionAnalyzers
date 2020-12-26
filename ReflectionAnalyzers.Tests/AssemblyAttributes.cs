67c13cf29f89db52ce86d7f92d90f220e4aae253
JohanLarsson <johan_gavle1@hotmail.com>
2020-12-26 16:14:19 +0100

    Fix warnings.

--- ReflectionAnalyzers.Tests/AssemblyAttributes.cs
+++ ReflectionAnalyzers.Tests/AssemblyAttributes.cs
@@ -1,4 +1,7 @@
+﻿using System;
 using Gu.Roslyn.Asserts;
+
+[assembly: CLSCompliant(false)]
 
 [assembly: MetadataReference(typeof(object), new[] { "global", "mscorlib" })]
 [assembly: MetadataReference(typeof(System.Diagnostics.Debug), new[] { "global", "System" })]
