e43f9cd4361aac5e46cba147c965124b35fd79df
JohanLarsson <johan_gavle1@hotmail.com>
2021-12-09 09:29:23 +0100

    Disable nullable in tests.
    See if we get a green build. Devops stopped running tests after switching to net6

--- ReflectionAnalyzers.Tests/ModuleInitializer.cs
+++ ReflectionAnalyzers.Tests/ModuleInitializer.cs
@@ -2,6 +2,7 @@
 {
     using System.Runtime.CompilerServices;
     using Gu.Roslyn.Asserts;
+    using Microsoft.CodeAnalysis;
 
     internal static class ModuleInitializer
     {
@@ -9,6 +10,7 @@
         internal static void Initialize()
         {
             Settings.Default = Settings.Default
+                                       .WithCompilationOptions(x => x.WithNullableContextOptions(NullableContextOptions.Disable))
                                        .WithMetadataReferences(MetadataReferences.Transitive(typeof(ModuleInitializer), typeof(System.Windows.Controls.Control), typeof(System.Windows.Forms.Control)));
         }
     }
